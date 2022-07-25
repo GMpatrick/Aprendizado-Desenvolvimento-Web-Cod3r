9a4f45d776f63d53b64d300b0b722ac2795bd428
GMpatrick <patrickalvessc@gmail.com>
2022-07-24 21:51:56 -0300

    Commits do dia 24

---
+++ node js/moduloCliente.js
@@ -0,0 +1,10 @@
+const moduloA = require('./moduloA')
+
+const moduloB = require('./moduloB')
+
+console.log(moduloA.bemVindo)
+
+console.log(moduloB.boaNoite())
+
+console.log(moduloB.soma(1, 1))
+
